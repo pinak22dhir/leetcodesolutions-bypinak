7c323b6ca8ed1c0a7bff2644f76a4caa50b34309
pinak dhir <pinakdhir@gmail.com>
2024-02-27 20:25:21 +0530

    2976 added

---
+++ 2976. mininimum_number_game.cpp
@@ -0,0 +1,16 @@
+ class Solution {
+    public:
+     vector<int> numberGame(vector<int>& nums) {
+        vector<int> arr(nums.size());
+ 
+   sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+           swap(nums[i],nums[i+1]);
+         }
+         for(int i=0;i<nums.size();i++){
+            arr[i]=nums[i];
+        }
+        // arr=nums;
+        return  arr;
+    }
+};
